2f15c5548c5bdffb18d66ffe957393baabce41b5
Saidali Saburov <ssaburov@velox.biz>
2020-04-19 16:58:34 +0500

    -init

---
+++ src/main/java/hello/SampleController.java
@@ -0,0 +1,29 @@
+package hello;
+/*
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/salom")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+
+    @RequestMapping("/kh")
+    @ResponseBody
+    String khona() {
+        return "Salom Alek";
+    }
+}*/
